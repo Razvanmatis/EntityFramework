618f4c05170d74b07aa245f99d2b5932937a47b7
r.matis.rm@gmail.com <r.matis.rm@gmail.com>
2020-10-27 19:55:42 +0100

    created project

---
+++ EntityFramework/EntityFramework/Models/FitnessDatabase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.Models
+{
+    public class FitnessDatabase : DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("SERVER=LOCALHOST\\SQLEXPRESS;Database=fitnessDatabase;Trusted_Connection=true");
+        }
+        //
+        // Zusammenfassung:
+        //     Override this method to further configure the model that was discovered by convention
+        //     from the entity types exposed in Microsoft.EntityFrameworkCore.DbSet`1 properties
+        //     on your derived context. The resulting model may be cached and re-used for subsequent
+        //     instances of your derived context.
+        //
+        // Parameter:
+        //   modelBuilder:
+        //     The builder being used to construct the model for this context. Databases (and
+        //     other extensions) typically define extension methods on this object that allow
+        //     you to configure aspects of the model that are specific to a given database.
+        //
+        // Hinweise:
+        //     If a model is explicitly set on the options for this context (via Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.UseModel(Microsoft.EntityFrameworkCore.Metadata.IModel))
+        //     then this method will not be run.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ EntityFramework/EntityFramework/Models/Member.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.Models
+{
+    public class Member
+    {
+        public long Id { get; set; }
+        public string Prename { get; set; }
+    }
+}
